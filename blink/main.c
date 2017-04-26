6844e1114ffcd2c2c5bc1dc90018b08cdbe0ffe3
Przemyslaw Kobylanski <przemko@mac.com>
2017-04-26 20:38:34 +0200

    poprawki

--- blink/main.c
+++ blink/main.c
@@ -1,17 +1,38 @@
 // main.c
-// Przyklad z ksiazki Elliot Williams "Make: AVR Programming"
 
 #include <avr/io.h>
+#include <avr/power.h>
 #include <util/delay.h>
 
-int main(void)
+int main()
 {
-   DDRB |= (1 << PB0);
-    for(;;){
-        PORTB = 0b00000001;
-        _delay_ms(1000);
-        PORTB = 0b00000000;
-        _delay_ms(1000);
+  clock_prescale_set(clock_div_1);
+  // nadawanie SOS
+  DDRB |= 0b00000001;
+  int t = 1200 / 20; // 1200 ms / words per minute
+  for(;;)
+    {
+      for(int i=0; i<3; i++)
+	{
+	  PORTB |= 0b00000001; _delay_ms(t);
+	  PORTB &= 0b11111110; _delay_ms(t);
+	}
+      _delay_ms(2*t);
+      
+      for(int i=0; i<3; i++)
+	{
+	  PORTB |= 0b00000001; _delay_ms(3*t);
+	  PORTB &= 0b11111110; _delay_ms(t);
+	}
+      _delay_ms(t);
+      
+      for(int i=0; i<3; i++)
+	{
+	  PORTB |= 0b00000001; _delay_ms(t);
+	  PORTB &= 0b11111110; _delay_ms(t);
+	}
+      _delay_ms(6*t);
     }
-    return 0;
+  return 0;
 }
+
