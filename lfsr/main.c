d55af305c44f0db25506eefc9c44ce09bcb6e839
Przemyslaw Kobylanski <przemko@mac.com>
2017-04-27 17:08:16 +0200

    poprawki

--- lfsr/main.c
+++ lfsr/main.c
@@ -8,28 +8,26 @@
 int main(void)
 {
   clock_prescale_set(clock_div_1);
-	const uint8_t addr = 0x71;
-	matrix8x8_init(addr);
-	matrix8x8_clear();
-
-	uint8_t lfsr = 0xA7;
-	uint8_t bit0, bit7;
-
+  const uint8_t addr = 0x71;
+  matrix8x8_init(addr);
+  matrix8x8_clear();
+  
+  uint8_t lfsr = 0x00;
+  uint8_t bit0, bit7;
+  
   DDRB |= 0b00000001;
-	for(;;)
-	{
-			bit0 = lfsr & 0x01;
-			bit7 =	((lfsr&64)<<1) ^
-							((lfsr&32)<<2) ^
-							((lfsr&8)<<4) ^
-							((lfsr&2)<<6);
-			lfsr = (bit7&128)|(lfsr>>1);
+  for(;;)
+    {
+      bit0 = lfsr & 0x01;
+      bit7 = ((lfsr&16)<<3)^((lfsr&8)<<4)^((lfsr&4)<<5)^((lfsr&1)<<7);
+      bit7 = ~bit7;
+      lfsr = (bit7&128)|(lfsr>>1);
       matrix8x8_scrolldown();
       matrix8x8_row(0, lfsr);
       matrix8x8_writedisplay(addr);
       PORTB |= bit0;
-      _delay_ms(25);
+      _delay_ms(100);
       PORTB &= 0b11111110;
-	}
+    }
   return 0;
 }
